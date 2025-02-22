c096cd61269f6d011b201f908da8d71473bac557
PrveenYadav <157797242+PrveenYadav@users.noreply.github.com>
2025-02-22 04:06:52 +0000

    01_basics

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all js code as newer version
+
+//Some Data types in javascript
+let name = "Vicky" //String
+let age = 18 //Number
+let isLoggin = false //Boolean
+let state; //undefined
+let num = null; //null
+
+console.log("Data type of age:", typeof age)
+//or
+console.log(typeof(age))
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
